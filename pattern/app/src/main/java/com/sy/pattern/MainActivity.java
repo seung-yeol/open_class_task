4bd25b4a8a2ddb9690058dd9b98e075a6a2a114d
seung-yeol <oks153123@naver.com>
2016-05-21 23:03:41 +0900

    패턴

---
+++ pattern/app/src/main/java/com/sy/pattern/MainActivity.java
@@ -0,0 +1,39 @@
+package com.sy.pattern;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    private PatternView patternView;
+
+    private String patternString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        patternView = (PatternView) findViewById(R.id.patternView);
+        Toast.makeText(getApplicationContext(), "ENTER PATTERN", Toast.LENGTH_LONG).show();
+        patternView.setOnPatternDetectedListener(new PatternView.OnPatternDetectedListener() {
+            
+            @Override
+            public void onPatternDetected() {
+                if (patternString == null) {
+                    patternString = patternView.getPatternString();
+//                    patternView.clearPattern();
+                    return;
+                }
+                if (patternString.equals(patternView.getPatternString())) {
+                    Toast.makeText(getApplicationContext(), "PATTERN CORRECT", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(getApplicationContext(), "PATTERN NOT CORRECT", Toast.LENGTH_SHORT).show();
+//                patternView.clearPattern();
+            }
+        });
+
+    }
+}
